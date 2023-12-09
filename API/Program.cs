37a953d75d37f2de2bf8231f73d7d2bf8bfa57eb
vijaybabar <babarvm@gmail.com>
2023-12-09 19:51:22 +0530

    Added docker file to build docker image Added global.json to set .net target version to 6 Fixed Dateonly issue by adding converter

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+               options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+            });
 
 var app = builder.Build();
 
@@ -23,3 +31,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+public sealed class DateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.FromDateTime(reader.GetDateTime());
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        var isoDate = value.ToString("O");
+        writer.WriteStringValue(isoDate);
+    }
+}
