cb8979eb79fdd646193267004b0621595c371f42
vijaybabar <babarvm@gmail.com>
2023-12-10 15:41:59 +0530

    Added logging

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -21,12 +21,17 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        _logger.LogInformation($"WeatherForecast Controller Get Method Invoke: Enter");
+        
+        var response = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        }).ToArray();
+
+        _logger.LogInformation($"Data Lenght: {response.Count()}");
+
+        return response;
     }
 }
